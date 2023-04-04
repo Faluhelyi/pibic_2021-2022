68abc50a224c1b60a94e895c1458179bf8c4358e
Faluhelyi <chalizard000@gmail.com>
2023-04-04 11:42:42 -0300

    Update files

--- solution/base_2.py
+++ solution/base_2.py
@@ -142,3 +142,48 @@ def get_exp_var():
                             'weekday_Wednesday'], axis = 1)
     
     return exp_var
+
+
+##################################################
+### Construção da função para calcular o sMAPE ###
+##################################################
+##################################################
+def calculate_smape(actual, forecast) -> float:
+    # Convert actual and forecast  to numpy
+    # array data type if not already
+    if not all([isinstance(actual, np.ndarray), 
+                isinstance(forecast, np.ndarray)]):
+         actual, forecast  = np.array(actual), np.array(forecast)
+  
+    return round(
+        np.mean(\
+        np.abs(forecast - actual) / ((np.abs(forecast) + np.abs(actual))/2))*100\
+            , 2)
+
+
+
+
+##################################################
+### Construção da função para calcular o MASE  ###
+##################################################
+##################################################
+def MASE(training_series, testing_series, forecast_series):
+    """
+    Computes the MEAN-ABSOLUTE SCALED ERROR forcast error for univariate time series forecast.
+    
+    See "Another look at measures of forecast accuracy", Rob J Hyndman
+    
+    parameters:
+        training_series: the series used to train the model, 1d numpy array
+        testing_series: the test series to predict, 1d numpy array or float
+        prediction_series: the prediction of testing_series, 1d numpy array (same size as testing_series) or float
+        absolute: "squares" to use sum of squares and root the result, "absolute" to use absolute values.
+    
+    """
+    print("Needs to be tested.")
+    
+    n = training_series.shape[0]
+    d = np.abs(np.diff(training_series)).sum()/(n-1)
+    
+    errors = np.abs(testing_series - forecast_series)
+    return errors.mean()/d
